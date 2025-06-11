40a4a006c5ced5d29dba15247d0ad84f51c2f19e
doohaaa <engk5423@naver.com>
2025-06-11 11:18:00 +0900

    feat: Lv1. 비밀번호 Bcrypt 로 암호화 구현

---
+++ src/main/java/com/example/newsfeedproject/config/PasswordEncoder.java
@@ -0,0 +1,20 @@
+package com.example.newsfeedproject.config;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncoder {
+
+    public PasswordEncoder passwordEncoder(){
+        return new PasswordEncoder();
+    }
+    public String encode(String rawPassword){
+        return BCrypt.withDefaults().hashToString(BCrypt.MIN_COST, rawPassword.toCharArray());
+    }
+
+    public boolean matches(String rawPassword, String encodedPassword){
+        BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
+        return result.verified;
+    }
+}
--- src/main/java/com/example/newsfeedproject/domain/user/UserService.java
+++ src/main/java/com/example/newsfeedproject/domain/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.newsfeedproject.domain.user;
 
+import com.example.newsfeedproject.config.PasswordEncoder;
 import com.example.newsfeedproject.domain.user.dto.SignUpUserResponseDto;
 import com.example.newsfeedproject.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -10,15 +11,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SignUpUserResponseDto signup(String email, String password){
-        User user = new User(email, password);
+        // User user = new User(email, password);
 
+        // 아이디 중복 확인
         if(userRepository.existsByEmail(email)){
             throw new IllegalArgumentException("이미 사용중인 아이디입니다.");
         }
-
-        User signupUser = userRepository.save(user);
+        User encryptedUser = new User(email,passwordEncoder.encode(password));
+        User signupUser = userRepository.save(encryptedUser);
         return new SignUpUserResponseDto(signupUser.getId(),signupUser.getEmail());
 
     }
